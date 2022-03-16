b49e211e91d99306f5a81e8e7c36bf2badbd3947
hongbin.wang <hongbin.wang@matrx.team>
2022-03-16 21:47:57 +0800

    compile error

--- api/crypto/crystal_packet_observer_register.cc
+++ api/crypto/crystal_packet_observer_register.cc
@@ -1,10 +1,23 @@
 #include "crystal_packet_observer_register.h"
 
 crystal::rtc::PacketObserver *packet_observer = nullptr;
-extern "C" void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver) {
-    packet_observer = packetObserver;
-}
 
+#if defined(WEBRTC_WIN)
+extern "C" void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver) {
+      packet_observer = packetObserver;
+}
+#else
+extern void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver) {
+      packet_observer = packetObserver;
+}
+#endif
+
+#if defined(WEBRTC_WIN)
 extern "C" void unRegisterPacketObserver() {
   packet_observer = NULL;
 }
+#else
+extern void unRegisterPacketObserver() {
+  packet_observer = NULL;
+}
+#endif

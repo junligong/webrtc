db4b912106c1bcabb2d2c8c45f4ab7f648a6c58b
hongbin.wang <hongbin.wang@matrx.team>
2022-03-17 14:23:21 +0800

    add extern "C"

--- api/crypto/crystal_packet_observer_register.cc
+++ api/crypto/crystal_packet_observer_register.cc
@@ -2,22 +2,10 @@
 
 crystal::rtc::PacketObserver *packet_observer = nullptr;
 
-#if defined(WEBRTC_WIN)
 extern "C" void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver) {
       packet_observer = packetObserver;
 }
-#else
-extern void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver) {
-      packet_observer = packetObserver;
-}
-#endif
 
-#if defined(WEBRTC_WIN)
 extern "C" void unRegisterPacketObserver() {
   packet_observer = NULL;
 }
-#else
-extern void unRegisterPacketObserver() {
-  packet_observer = NULL;
-}
-#endif
--- api/crypto/crystal_packet_observer_register.h
+++ api/crypto/crystal_packet_observer_register.h
@@ -1,14 +1,6 @@
 #include "api/crypto/crystal_packet_observer.h"
 #include "rtc_base/system/rtc_export.h"
 
-#if defined(WEBRTC_WIN)
 extern "C" void registerPacketObserver(crystal::rtc::PacketObserver *packetObserver);
-#else
-extern void RTC_EXPORT registerPacketObserver(crystal::rtc::PacketObserver *packetObserver);
-#endif
 
-#if defined(WEBRTC_WIN)
 extern "C" void unRegisterPacketObserver();
-#else
-extern void RTC_EXPORT unRegisterPacketObserver();
-#endif
